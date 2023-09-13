c7c577a1cffe735c142e8e8293bff248239aa87e
PolakitT <144783393+PolakitT@users.noreply.github.com>
2023-09-13 13:57:11 +0700

    Add files via upload

---
+++ Exercise4_Polakit_T.py
@@ -0,0 +1,10 @@
+English     = 78.5
+Business    = 75.0
+ComputerSys = 80.0
+ComputerPrg = 81.5
+
+print("--- Your Score ---")
+print("Foundation English               : ",English)
+print("General Business                 : ",Business)
+print("Introduction to Computer System  : ",ComputerSys)
+print("Computer Programming             : ",ComputerPrg)
